a809ffc4f36062a1451be0c5d7895c2ff246d3c8
Rithanya89 <rithanyamohanraj29@gmail.com>
2025-09-27 11:23:36 +0530

    Update PRO99.php

--- PRO99.php
+++ PRO99.php
@@ -7,4 +7,11 @@ if(!isset($_SESSION["count"])) {
 }
 echo "Visit count: " . $_SESSION["count"];
 
-
+$target = "uploads/" . basename($_FILES["file"]["name"]);
+if(file_exists($target)) {
+    echo "Error: File already exists.";
+} else {
+    move_uploaded_file($_FILES["file"]["tmp_name"], $target);
+    echo "File uploaded.";
+}
+?>
